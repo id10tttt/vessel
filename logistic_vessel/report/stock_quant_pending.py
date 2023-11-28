01f46350e331269f90951d822b9b44ff4504b36d
1di0t <daiguangquan@sf-pharma.com>
2023-11-29 00:35:44 +0800

    调整

--- logistic_vessel/report/stock_quant_pending.py
+++ logistic_vessel/report/stock_quant_pending.py
@@ -9,28 +9,60 @@ class StockQuantPending(models.Model):
     _auto = False
     _order = 'id desc, warehouse_enter_no'
 
-    stock_in_order = fields.Many2one('sale.order', string='Stock IN')
+    stock_in_order = fields.Many2one('sale.order', string='Stock IN', compute='_compute_stock_in_order_info')
     stock_out_order = fields.Many2one('sale.order', string='Stock OUT', compute='_compute_stock_out_order_info')
-    supplier_id = fields.Many2one('res.partner', string='Supplier')
-    mv = fields.Many2one('freight.vessel', string='M/V')
+    supplier_id = fields.Many2one('res.partner', string='Supplier', compute='_compute_stock_in_order_info')
+    mv = fields.Many2one('freight.vessel', string='M/V', compute='_compute_stock_in_order_info')
     lot_id = fields.Many2one('stock.lot', string='Lot')
     package_id = fields.Many2one('stock.quant.package', string='Package')
-    owner_ref = fields.Char('Ref#')
-    location = fields.Char('Location')
-    warehouse_enter_no = fields.Char('Warehouse Enter No')
+    owner_ref = fields.Char('Ref#', related='lot_id.name')
+    location = fields.Char('Location', compute='_compute_stock_in_order_info')
+    warehouse_enter_no = fields.Char('Warehouse Enter No', compute='_compute_stock_in_order_info')
     quantity = fields.Float('Quantity')
     reserved_quantity = fields.Float('Reserved Quantity')
-    weight = fields.Float('Weight')
-    volume = fields.Float('Volume')
-    dimensions = fields.Char('Dimensions(LxMxH cm)')
-    ready_date = fields.Date('Ready Date')
-    arrival_date = fields.Date('Arrival Date')
-    invoice = fields.Char('Invoice')
-    so = fields.Char('S/O')
+    weight = fields.Float('Weight', related='package_id.gross_weight_pc')
+    volume = fields.Float('Volume', related='package_id.volume')
+    dimensions = fields.Char('Dimensions(LxMxH cm)', related='package_id.dimensions')
+    ready_date = fields.Date('Ready Date', compute='_compute_stock_in_order_info')
+    arrival_date = fields.Date('Arrival Date', compute='_compute_stock_in_order_info')
+    invoice = fields.Char('Invoice', compute='_compute_stock_in_order_info')
+    your_ref = fields.Char('Your Ref', compute='_compute_stock_in_order_info')
     dest = fields.Char('Dest', compute='_compute_stock_out_order_info')
     awb = fields.Char('Awb', compute='_compute_stock_out_order_info')
     departure_date = fields.Date('Departure Date', compute='_compute_stock_out_order_info')
     ref = fields.Char('Ref', compute='_compute_stock_out_order_info')
+
+    @api.depends('owner_ref', 'lot_id', 'package_id')
+    def _compute_stock_in_order_info(self):
+        sale_order_ids = self.env['sale.order'].sudo().search([
+            ('order_type', '=', 'stock_in'),
+            ('owner_ref', 'in', self.lot_id.mapped('name')),
+            ('state', '!=', 'cancel')
+        ])
+        for quant_id in self:
+            order_id = sale_order_ids.filtered(lambda so: so.owner_ref == quant_id.lot_id.name)
+            if order_id:
+                picking_ids = order_id.picking_ids
+                valid_picking_id = picking_ids.filtered(lambda p: p.state == 'done')
+                quant_id.stock_in_order = order_id[0].id
+                quant_id.supplier_id = order_id[0].partner_id.id
+                quant_id.mv = order_id[0].mv.id
+                quant_id.location = order_id[0].location_id.name
+                quant_id.warehouse_enter_no = order_id[0].warehouse_enter_no
+                quant_id.ready_date = order_id[0].date_order
+                quant_id.arrival_date = valid_picking_id[0].date_done if valid_picking_id else None
+                quant_id.invoice = order_id[0].message_attachment_urls
+                quant_id.your_ref = order_id[0].client_order_ref
+            else:
+                quant_id.stock_in_order = None
+                quant_id.supplier_id = None
+                quant_id.mv = None
+                quant_id.location = None
+                quant_id.warehouse_enter_no = None
+                quant_id.ready_date = None
+                quant_id.arrival_date = valid_picking_id[0].date_done if valid_picking_id else None
+                quant_id.invoice = None
+                quant_id.your_ref = None
 
     @api.depends('owner_ref', 'lot_id', 'package_id')
     def _compute_stock_out_order_info(self):
@@ -65,130 +97,14 @@ class StockQuantPending(models.Model):
 
     def get_query(self):
         query = """
-        with pure_stock_in as (with stock_in as (select sq.id                 as id,
-                                                        so.id                 as stock_in_order,
-                                                        sq.lot_id             as lot_id,
-                                                        sq.package_id         as package_id,
-                                                        rp.id                 as supplier_id,
-                                                        so.mv                 as mv,
-                                                        sqp.gross_weight_pc   as weight,
-                                                        sqp.volume            as volume,
-                                                        sqp.dimensions        as dimensions,
-                                                        so.owner_ref          as owner_ref,
-                                                        rcs.name              as location,
-                                                        so.warehouse_enter_no as warehouse_enter_no,
-                                                        so.date_order         as ready_date,
-                                                        sq.quantity           as quantity,
-                                                        sq.reserved_quantity  as reserved_quantity,
-                                                        so.invoice_no         as invoice,
-                                                        so.client_order_ref   as so,
-                                                        ''                    as dest,
-                                                        ''                    as awb,
-                                                        ''                    as departure_date,
-                                                        ''                    as ref,
-                                                        so.arrival_date       as arrival_date
-                                                 from stock_quant as sq
-                                                          left join stock_location sl2 on
-                                                     sq.location_id = sl2.id
-                                                          left join stock_quant_package sqp on
-                                                     sq.package_id = sqp.id
-                                                          left join stock_lot sl on
-                                                     sq.lot_id = sl.id
-                                                          left join sale_order so on
-                                                     sl.name = so.owner_ref
-                                                          left join res_partner rp on
-                                                     rp.id = so.partner_id
-                                                          left join res_country_state rcs on
-                                                     rcs.id = so.location_id
-                                                 where (so.state = 'sale'
-                                                     or so.state is null)
-                                                   and sq.package_id is not null
-                                                   and so.order_type = 'stock_in'
-                                                   and sl2.usage = 'internal'
-                                                 union
-                                                 select sq.id                 as id,
-                                                        so.id                 as stock_in_order,
-                                                        sq.lot_id             as lot_id,
-                                                        sq.package_id         as package_id,
-                                                        rp.id                 as supplier_id,
-                                                        so.mv                 as mv,
-                                                        sqp.gross_weight_pc   as weight,
-                                                        sqp.volume            as volume,
-                                                        sqp.dimensions        as dimensions,
-                                                        so.owner_ref          as owner_ref,
-                                                        rcs.name              as location,
-                                                        so.warehouse_enter_no as warehouse_enter_no,
-                                                        so.date_order         as ready_date,
-                                                        sq.quantity           as quantity,
-                                                        sq.reserved_quantity  as reserved_quantity,
-                                                        so.invoice_no         as invoice,
-                                                        so.client_order_ref   as so,
-                                                        ''                    as dest,
-                                                        ''                    as awb,
-                                                        ''                    as departure_date,
-                                                        ''                    as ref,
-                                                        so.arrival_date       as arrival_date
-                                                 from stock_quant as sq
-                                                          left join stock_location sl2 on
-                                                     sq.location_id = sl2.id
-                                                          left join stock_quant_package sqp on
-                                                     sq.package_id = sqp.id
-                                                          left join stock_lot sl on
-                                                     sq.lot_id = sl.id
-                                                          left join sale_order so on
-                                                     sl.name = so.owner_ref
-                                                          left join res_partner rp on
-                                                     rp.id = so.partner_id
-                                                          left join res_country_state rcs on
-                                                     rcs.id = so.location_id
-                                                 where (so.state = 'sale'
-                                                     or so.state is null)
-                                                   and sq.package_id is null
-                                                   and so.order_type = 'stock_in'
-                                                   and sl2.usage = 'internal')
-                               select si.id                 as id,
-                                      si.stock_in_order     as stock_in_order,
-                                      cast(null as bigint)  as stock_out_order,
-                                      si.lot_id             as lot_id,
-                                      si.package_id         as package_id,
-                                      si.supplier_id        as supplier_id,
-                                      si.mv                 as mv,
-                                      si.weight             as weight,
-                                      si.volume             as volume,
-                                      si.dimensions         as dimensions,
-                                      si.owner_ref          as owner_ref,
-                                      si.location           as location,
-                                      si.warehouse_enter_no as warehouse_enter_no,
-                                      si.ready_date         as ready_date,
-                                      si.quantity           as quantity,
-                                      si.reserved_quantity  as reserved_quantity,
-                                      si.invoice            as invoice,
-                                      si.so                 as so,
-                                      ''                    as dest,
-                                      ''                    as awb,
-                                      ''                    as departure_date,
-                                      ''                    as ref,
-                                      si.arrival_date       as arrival_date
-                               from stock_in si)
-        select pure_stock_in.id                 as id,
-               pure_stock_in.stock_in_order     as stock_in_order,
-               pure_stock_in.lot_id             as lot_id,
-               pure_stock_in.package_id         as package_id,
-               pure_stock_in.supplier_id        as supplier_id,
-               pure_stock_in.mv                 as mv,
-               pure_stock_in.weight             as weight,
-               pure_stock_in.volume             as volume,
-               pure_stock_in.dimensions         as dimensions,
-               pure_stock_in.owner_ref          as owner_ref,
-               pure_stock_in.location           as location,
-               pure_stock_in.warehouse_enter_no as warehouse_enter_no,
-               pure_stock_in.ready_date         as ready_date,
-               pure_stock_in.quantity           as quantity,
-               pure_stock_in.reserved_quantity  as reserved_quantity,
-               pure_stock_in.invoice            as invoice,
-               pure_stock_in.so                 as so,
-               pure_stock_in.arrival_date       as arrival_date
-        from pure_stock_in
+        select sq.id                as id,
+               sq.lot_id            as lot_id,
+               sq.package_id        as package_id,
+               sq.quantity          as quantity,
+               sq.reserved_quantity as reserved_quantity
+        from stock_quant as sq
+                 join stock_location sl on sq.location_id = sl.id
+        where sl.usage = 'internal'
         """
         return query
 
